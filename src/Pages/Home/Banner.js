89df9c95f9916913d653456e87b321e8dd475032
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-10 03:03:24 +0600

    banner sectiion added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import chair from '../../assets/images/chair.png'
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen ">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img className='w-[594px] h-[763]' src={chair} />
+                <div>
+                    <h1 class="text-6xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
