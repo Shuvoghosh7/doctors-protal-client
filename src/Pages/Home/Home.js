b7fd697db3586305a44ddb9582e75c991a609444
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-12 01:42:36 +0600

    add footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 import MakeAppointment from '../../MakeAppointment/MakeAppointment';
 import OurServices from '../OurService/OurServices';
 import Banner from './Banner';
+import Contact from './Contact/Contact';
 import DentalWellcome from './DentalWellcome/DentalWellcome';
+import Footer from './Footer/Footer';
 import Info from './Info/Info';
+import Testimonials from './Testimonials/Testimonials';
 
 
 const Home = () => {
@@ -14,6 +17,9 @@ const Home = () => {
             <OurServices/>
             <DentalWellcome/>
             <MakeAppointment/>
+            <Testimonials/>
+            <Contact/>
+            <Footer/>
         </div>
     );
 };
